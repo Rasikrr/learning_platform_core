f562d07dd7515f5dea44dbafbd5c51c12f0cd461
Rasikrr <rs.t.95@mail.ru>
2025-03-09 16:48:45 +0500

    converters for time in grpc

--- grpc/converters/timestamp.go
+++ grpc/converters/timestamp.go
@@ -5,17 +5,25 @@ import (
 	"time"
 )
 
-func ConvertToTimestamp(t *time.Time) *timestamppb.Timestamp {
+func ConvertToTimestampPb(t *time.Time) *timestamppb.Timestamp {
 	if t == nil {
 		return nil
 	}
 	return timestamppb.New(*t)
 }
 
-func ConvertFromTimestamp(t *timestamppb.Timestamp) *time.Time {
+func ConvertToTimePtr(t *timestamppb.Timestamp) *time.Time {
 	if t == nil {
 		return nil
 	}
 	out := t.AsTime()
 	return &out
+}
+
+func ConvertToTime(t *timestamppb.Timestamp) time.Time {
+	if t == nil {
+		return time.Time{}
+	}
+	out := t.AsTime()
+	return out
 }
